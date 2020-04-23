4875b84b3904929c93c110e7825934663c7596fc
unknown <prakashbahadursingh2@gmail.com>
2020-04-23 13:44:16 -0700

    react, axios, yup dpendencies imprted also form and UserCard component imported

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import axios from 'axios'
+import *as yup from 'yup'
+
+import Form from './Form'
+import UserCard from './UserCard'
+
+const url = 'https://reqres.in/api/users'
+
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
